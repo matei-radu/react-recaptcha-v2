5952db6769ae7b48150936872eb1c4156990135b
Matei Radu <matei.radu.92@gmail.com>
2018-10-05 16:40:44 +0200

    Add Rollup to setup

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2018-present, Matei Bogdan Radu <matei.radu.92@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+import typescript from "rollup-plugin-typescript";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "lib/index.esm.js",
+    format: "esm"
+  },
+  plugins: [typescript()]
+};
