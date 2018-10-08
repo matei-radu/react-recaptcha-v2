88548be9ee14cf6ad916ab8bb860fa0c625aedfc
Matei Radu <matei.radu.92@gmail.com>
2018-10-08 11:16:51 +0200

    Add methods to append script to the DOM

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,50 @@
 
 import React, { Component } from "react";
 
-class ReCaptcha extends Component {
+interface ReCaptchaProps {
+  siteKey: string,
+}
+
+class ReCaptcha extends Component<ReCaptchaProps, undefined> {
+  private scriptSrc = "https://www.google.com/recaptcha/api.js";
+
+  /**
+   * Appends the reCAPTCHA script to the document body if necessary.
+   */
+  appendScript() {
+    if (!this.isScriptAvailable()) {
+      const reCaptchaScript = this.createScriptElement();
+      document.body.appendChild(reCaptchaScript)
+    }
+  }
+
+  isScriptAvailable(): boolean {
+    if (document.getElementById('recaptcha')) {
+      return true;
+    }
+
+    // Maybe the script was added but not from this component.
+    const availableScripts = Array.from(document.scripts);
+    availableScripts.forEach(script => {
+      if (script.src === this.scriptSrc) {
+        return true;
+      }
+    });
+
+    return false;
+  }
+
+  createScriptElement(): HTMLScriptElement {
+    const reCaptchaScript = document.createElement("script");
+    reCaptchaScript.id = "recaptcha";
+    reCaptchaScript.src = this.scriptSrc;
+    reCaptchaScript.async = true;
+    reCaptchaScript.defer = true;
+
+    return reCaptchaScript;
+  }
+
+
   render() {
     return <div>ReCaptcha</div>
   }
